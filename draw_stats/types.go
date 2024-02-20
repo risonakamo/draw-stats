9cb6a3c962454aa0551af1165e9154bbc12e96bf
risonakamo <hmykanae@gmail.com>
2024-02-20 02:53:51 -0500

    rework to typings

--- draw_stats/types.go
+++ draw_stats/types.go
@@ -2,51 +2,47 @@ package draw_stats
 
 import "time"
 
-// map of category stats. keyed by the category name
-type CategoryStats map[string]CategoryStat
+// identifier tags for a time event.
+// key: name of the tag
+// val: the tag value
+type TagsDict map[string]string
 
-// single draw row event
-type DrawRow struct {
-	Item string
+// typed version of tags map
+type TagsDictTyped struct {
+    Item string
     Category string
+    Date time.Time
+}
 
-	Start time.Time
+// a single event. a full log is just a list of time events
+type TimeEvent struct {
+    Tags TagsDictTyped
+
+    Start time.Time
     End time.Time
 
     Duration time.Duration
 }
 
-type DrawStats struct {
-    Rows []DrawRow
 
+
+
+// ---- collection structures ----
+// analysis of a list of time events. corresponds with a list of time events
+type TimeEventAnalysis struct {
     TotalTime time.Duration
+    AverageTime time.Duration
 
-    // stats per day. keyed by the date
-    DayStats map[time.Time]DayStat
-    // average time over all days
-    AverageTimePerDay time.Duration
-
-    // stats per single item. keyed by item name
-    ItemStats map[string]ItemStat
-
-    // stats per category. keyed by category name
-    CategoryStats CategoryStats
+    Events []TimeEvent
 }
 
-type DayStat struct {
-    Day time.Time
-    Time time.Duration
-}
+// analysis of a list of events. focusing on a single tag, the events are grouped by the tag's
+// unique values. then, for each unique value, stats are calculated
+type TagBreakdown struct {
+    Tag string
 
-type ItemStat struct {
-    Item string
-    TotalTime time.Duration
-
-    // breakdown of categories for the item
-    CategoryStats CategoryStats
-}
-
-type CategoryStat struct {
-    Category string
-    TotalTime string
+    // analysis for each tag value
+    // key: tag value
+    // val: the analysis
+    ValuesAnalysis map[string]TimeEventAnalysis
 }
